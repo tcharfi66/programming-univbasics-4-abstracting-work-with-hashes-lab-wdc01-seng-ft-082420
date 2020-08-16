40377fd63649d4ee63ed9177451b08f6d24fba04
Tej Charfi <charfi@usc.edu>
2020-08-16 20:51:42 +0000

    Automatically backed up by Learn

--- lib/intro_to_ruby_hashes_lab.rb
+++ lib/intro_to_ruby_hashes_lab.rb
@@ -1,19 +1,27 @@
 def my_hash_creator(key, value)
   # return a hash that includes the key and value parameters passed into this method
   
+<<<<<<< HEAD
   h= {
     key => value
   }
 
+=======
+  h= {:key=> key, :value=> value}
+>>>>>>> 2128557584ae54680a9397cde337114d0677fd5a
 end
 
 def read_from_hash(hash, key)
   # return the correct value using the hash and key parameters
+<<<<<<< HEAD
   if hash[key] != nil 
       return hash[key]
   end
   return nil
   
+=======
+  hash[key]
+>>>>>>> 2128557584ae54680a9397cde337114d0677fd5a
 end
 
 def update_counting_hash(hash, key)
@@ -25,8 +33,13 @@ def update_counting_hash(hash, key)
   if hash[key]
     hash[key]+=1
   else 
+<<<<<<< HEAD
      hash[key]= 1
   end
   
   hash
+=======
+    hash[key]= 1
+  end
+>>>>>>> 2128557584ae54680a9397cde337114d0677fd5a
 end
